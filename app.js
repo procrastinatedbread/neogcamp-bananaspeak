6605a8aae7b887a40256519afb29bea31a006615
NIkhil Chhabra <nikhillchhabra@gmail.com>
2022-07-21 19:01:46 +0530

    imput output processing done

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 console.log(txtInput);
 
 function clickhandler() {
-  console.log("clicked!");
-  console.log("input", txtInput.value);
+  outputDiv.innerText = "asfsfsfsffs    " + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickhandler);
