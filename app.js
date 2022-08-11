7d52091443af51b2dc4be0c2d80244910b2d676b
NIkhil Chhabra <nikhillchhabra@gmail.com>
2022-08-12 00:08:41 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with the server, try again after some time");
+}
 
 function clickhandler() {
-  outputDiv.innerText = "asfsfsfsffs    " + txtInput.value;
+  //   outputDiv.innerText = "asfsfsfsffs    " + txtInput.value;
+  var inputText = txtInput.value; //taking input
+
+  //calling server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
 }
 
 btnTranslate.addEventListener("click", clickhandler);
